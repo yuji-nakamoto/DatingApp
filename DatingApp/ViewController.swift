42c12553ffa345c1dfd40291f08c3007b743125d
yuji-nakamoto <yuji0411@icloud.com>
2020-07-21 02:16:44 +0900

    Initial Commit

---
+++ DatingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DatingApp
+//
+//  Created by yuji_nakamoto on 2020/07/21.
+//  Copyright © 2020 yuji_nakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
